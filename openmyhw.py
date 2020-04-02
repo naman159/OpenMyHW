bcea1bbf29500760cf067c35a91ab947211b00f8
naman ranawat <naman159@namans-MacBook-Pro.local>
2020-04-02 10:33:14 +0530

    Stable

--- openmyhw.py
+++ openmyhw.py
@@ -15,31 +15,39 @@ def main():
         driver = webdriver.Chrome()
         wait = WebDriverWait(driver, 10)
         driver.get("https://webwork.math.umass.edu/webwork2/S20_STAT_515_4/")   #The link to the login of that homework page
-        driver.find_element_by_id("uname").send_keys("your username/email")                        #username/email
-        driver.find_element_by_id("pswd").send_keys("your password")                         #password
+        driver.find_element_by_id("uname").send_keys("")                        #username/email
+        driver.find_element_by_id("pswd").send_keys("")                         #password
         driver.find_element_by_id("none").click()                               #Login, Sign in or submmit button
         print("logged in to stats 515 webwork!")
-        input1 = input()
+        main()
 
 
     elif(input1 == "5152"):                                                     #repeat, implementation for cengage webassign courses
         driver = webdriver.Chrome()
         wait = WebDriverWait(driver, 10)
         driver.get("https://www.webassign.net/wa-auth/login")
-        driver.find_element_by_id("email").send_keys("your username/email")
-        driver.find_element_by_id("cengagePassword").send_keys("your password")
+        driver.find_element_by_id("email").send_keys("")
+        driver.find_element_by_id("cengagePassword").send_keys("")
         driver.find_element_by_name("Login").click()
         print("logged in to stats 515 cengage!")
-        input1 = input()
+        main()
 
     elif(input1 == "235"):                                                      #repeat, implementation for Pearson - MyLab, MasteringPhysics courses
         driver = webdriver.Chrome()
         wait = WebDriverWait(driver, 10)
         driver.get("https://pi.pearsoned.com/v1/piapi/piui/signin?client_id=dN4bOBG0sGO9c9HADrifwQeqma5vjREy&okurl=https:%2F%2Fportal.mypearson.com%2Fcourse-home&siteid=8313")
-        driver.find_element_by_id("username").send_keys("your username/email")
-        driver.find_element_by_id("password").send_keys("your password")
+        driver.find_element_by_id("username").send_keys("")
+        driver.find_element_by_id("password").send_keys("")
         driver.find_element_by_id("mainButton").click()
         print("logged in to Math 235 MyLab!")
-        input1 = input()
+        main()
+
+    elif(input1 == "close"):
+        print("Enjoy your homework :)")
+
+
+    else:
+        print("Wrong command!")
+        main()
 
 main()
